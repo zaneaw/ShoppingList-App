49af957f6af86ce6036fc8aa361e97f32d17bacd
Zane Wilson <wilsonzanea@gmail.com>
2022-09-30 10:02:42 -0400

    formatting

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -5,21 +5,20 @@ import Icon from 'react-native-vector-icons/Feather';
 export default function ListItem({item}) {
   return (
     <TouchableOpacity style={styles.listItem}>
-        <Text style={styles.text}>{item.text}</Text>
-        <Icon name="trash-2" size={20} color='firebrick' />
+      <Text style={styles.text}>{item.text}</Text>
+      <Icon name="trash-2" size={20} color="firebrick" />
     </TouchableOpacity>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    listItem: {
-        paddingHorizontal: 20,
-        paddingVertical: 15,
-        borderTopWidth: 1,
-        borderBottomWidth: 1,
-        borderColor: '#eee',
-        backgroundColor: '#'
-    },
-    text: {
-    },
+  listItem: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    borderColor: '#eee',
+    backgroundColor: '#',
+  },
+  text: {},
 });
